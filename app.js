a5c3c7a5ecabdb3b76562cdc81a470a0744c1d8b
Mike Peiman <mikepeiman@gmail.com>
2018-07-02 10:16:06 -0700

    Refactored to derive 'to' and 'from' user object instances from dropdown user names as strings

--- app.js
+++ app.js
@@ -3,13 +3,75 @@ const subheading = document.getElementById('lesson-subheading')
 heading.textContent = 'ChatRoom App'
 subheading.textContent = 'Based on JS Mediator Pattern'
 
+const addUserBtn = document.getElementById('add-user')
+const fromUser = document.getElementById('from-user')
+const dropdownFrom = document.getElementById('dropdown-from')
+const dropdownTo = document.getElementById('dropdown-to')
+const dropdownMenu = document.querySelector('.dropdown-menu')
+const toUser = document.getElementById('to-user')
+const allUsers = document.getElementById('all-users')
+const selectAllUsers = document.getElementById('select-all-users')
+const messageList = document.getElementById('message-list')
+const submit = document.getElementById('submit')
+const messageField = document.getElementById('message')
+
+submit.addEventListener('click', function(e) {
+  let message = messageField.value
+  let from = fromUser.textContent.toLocaleLowerCase()
+  let to = toUser.textContent.toLocaleLowerCase()
+  console.log(from, to)
+  chatroom.send(message, eval(from), eval(to))
+  // sendMessage()
+  // chatroom.send(message, window[fromUser.textContent], jeff)
+
+  clearMessage()
+
+  e.preventDefault()
+})
+
+dropdownFrom.addEventListener('click', function(e) {
+  console.log('dropdownFrom', e.target.textContent)
+  fromUser.textContent = e.target.textContent
+})
+dropdownTo.addEventListener('click', function(e) {
+  console.log('dropdownTo', e.target.textContent)
+  toUser.textContent = e.target.textContent
+})
+
+function clearMessage() {
+  messageField.value = ''
+  messageField.focus()
+}
+
+
+
+
+// allUsers.childNodes[3].style.color = 'white'
+// console.log(allUsers.childNodes)
+
+
 // Allows us to subscribe/unsubscribe for events
 // can notify DOM of elements to update
 
 // const insert = document.getElementById('insertion-point')
 // const content = ''
 
-insert.innerHTML = content
+// insert.innerHTML = content
+
+// class UserES6 {
+//   constructor(name) {
+//     this.name = name
+//     this.chatroom = null
+//   }
+
+//   send(message, to) {
+//     this.chatroom.send(message, this, to)
+//   }
+
+//   receive(message, from) {
+//     console.log(`From ${from.name} to ${this.name}: ${message}`)
+//   }
+// }
 
 const UserES5 = function(name) {
   this.name = name
@@ -22,24 +84,10 @@ UserES5.prototype = {
   },
   receive: function(message, from) {
     console.log(`From ${from.name} to ${this.name}: ${message}`)
+    messageList.innerHTML += `
+    <li class="list-group-item">From ${from.name} to ${this.name}: ${message}</li>`
   }
 }
-
-class UserES6 {
-  constructor(name) {
-    this.name = name
-    this.chatroom = null
-  }
-
-  send(message, to) {
-    this.chatroom.send(message, this, to)
-  }
-
-  receive(message, from) {
-    console.log(`From ${from.name} to ${this.name}: ${message}`)
-  }
-}
-
 
 const ChatRoom = function() {
   let users = {} // list of users
@@ -48,6 +96,14 @@ const ChatRoom = function() {
     register: function(user) {
       users[user.name] = user
       user.chatroom = this
+      let dropdownItem = document.createElement('li')
+      dropdownItem.className = 'dropdown-item'
+      dropdownItem.textContent = user.name
+      dropdownFrom.appendChild(dropdownItem)
+      let dropdownItem2 = document.createElement('li')
+      dropdownItem2.className = 'dropdown-item'
+      dropdownItem2.textContent = user.name
+      dropdownTo.appendChild(dropdownItem2)
     },
     send: function(message, from, to) {
       if(to) {
@@ -65,23 +121,38 @@ const ChatRoom = function() {
   }
 }
 
+
+const chatroom = new ChatRoom()
+
+
+function addUser(name) {
+    this.name = new UserES5(name)
+    chatroom.register(this.name)
+}
+addUser('x')
+
 const brad = new UserES5('Brad')
 const jeff = new UserES5('Jeff')
 const sara = new UserES5('Sara')
-const mike = new UserES6('Mike')
-const melissa = new UserES6('Melissa')
-const kaya = new UserES6('Kaya')
+const michael = new UserES5('Michael')
+// const mike = new UserES6('Mike')
+// const melissa = new UserES6('Melissa')
+// const kaya = new UserES6('Kaya')
 
-const chatroom = new ChatRoom()
 
 chatroom.register(brad)
 chatroom.register(jeff)
 chatroom.register(sara)
-chatroom.register(mike)
-chatroom.register(kaya)
-chatroom.register(melissa)
+// chatroom.register(mike)
+// chatroom.register(kaya)
+// chatroom.register(melissa)
 
-brad.send('Hi there Jeff', jeff)
-sara.send('Yo Brad, whatsup?', brad)
-jeff.send('Hey everyone, how\'s it going?')
-mike.send('I love you baby!', kaya)
+function sendMessage() {
+  console.log(message)
+  chatroom.send(message.value, brad, jeff)
+}
+
+// brad.send('Hi there Jeff', jeff)
+// sara.send('Yo Brad, whatsup?', brad)
+// jeff.send('Hey everyone, how\'s it going?')
+// // mike.send('I love you baby!', kaya)
